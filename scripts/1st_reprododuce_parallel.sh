2882dcedad889afb2a72fb8f23772bc1e4c05830
mjs <mjs8612@gmail.com>
2025-01-20 18:49:44 +0000

    Add script to reproduce experiments with FedAvg and Scaffold algorithms

---
+++ scripts/1st_reprododuce_parallel.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+for partition in noniid-labeldir
+do
+    for alg in fedavg scaffold
+    do
+        python experiments.py --model=simple-cnn \
+            --dataset=cifar10 \
+            --alg=$alg \
+            --lr=0.01 \
+            --batch-size=64 \
+            --epochs=10 \
+            --n_parties=10 \
+            --rho=0.9 \
+            --comm_round=50 \
+            --partition=$partition \
+            --beta=0.5 \
+            --device='cuda:0' \
+            --datadir='./data/' \
+            --logdir="./logs/${alg}" \
+            --noise=0 \
+            --sample=1.0 \
+            --init_seed=0 &
+        sleep 10  # Add a 10-second delay between launches
+    done
+done
