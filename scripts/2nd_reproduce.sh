3871f6a146258e2ccd8f3ab5691a363f368ef48d
mjs <mjs8612@gmail.com>
2025-01-20 22:20:57 +0000

    Refactor experiment script to streamline model and algorithm execution; consolidate loops and improve logging structure

--- scripts/2nd_reproduce.sh
+++ scripts/2nd_reproduce.sh
@@ -1,47 +1,26 @@
-for model in vgg resnet
+#!/bin/bash
+
+model="vgg"
+partition="noniid-labeldir"
+beta=0.1
+
+for alg in scaffold fedavg
 do
-	for partition in noniid-labeldir 
-	do
-		for alg in fedavg fedprox scaffold fednova
-		do
-			python experiments.py --model=$model \
-				--dataset=cifar10 \
-				--alg=$alg \
-				--lr=0.01 \
-				--batch-size=64 \
-				--epochs=10 \
-				--n_parties=10 \
-				--rho=0.9 \
-				--mu=0.01 \
-				--comm_round=100 \
-				--partition=$partition \
-				--beta=0.1\
-				--device='cuda:0'\
-				--datadir='./data/' \
-				--logdir='./logs/' \
-				--noise=0\
-				--init_seed=0
-		done
-	done
-	
-	for alg in fedavg fedprox scaffold fednova
-		do
-			python experiments.py --model=$model \
-				--dataset=cifar10 \
-				--alg=$alg \
-				--lr=0.01 \
-				--batch-size=64 \
-				--epochs=10 \
-				--n_parties=10 \
-				--rho=0.9 \
-				--mu=0.01 \
-				--comm_round=100 \
-				--partition=homo \
-				--beta=0.1\
-				--device='cuda:0'\
-				--datadir='./data/' \
-				--logdir='./logs/' \
-				--noise=0.1\
-				--init_seed=0
-		done
+    python experiments.py --model=$model \
+        --dataset=cifar10 \
+        --alg=$alg \
+        --lr=0.01 \
+        --batch-size=64 \
+        --epochs=10 \
+        --n_parties=10 \
+        --rho=0.9 \
+        --mu=0.01 \
+        --comm_round=100 \
+        --partition=$partition \
+        --beta=$beta \
+        --device='cuda:0' \
+        --datadir='./data/' \
+        --logdir="./logs/$model/$alg/" \
+        --noise=0 \
+        --init_seed=0
 done
