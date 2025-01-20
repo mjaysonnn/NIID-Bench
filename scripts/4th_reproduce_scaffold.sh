149385e7f41864d9780d1e106a3619e15c91b30a
mjs <mjs8612@gmail.com>
2025-01-20 23:23:08 +0000

    Update reproduction scripts for FedAvg and Scaffold algorithms; change partition type to homo, update model to ResNet, and adjust logging parameters

--- scripts/4th_reproduce_scaffold.sh
+++ scripts/4th_reproduce_scaffold.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for partition in noniid-labeldir
+for partition in homo
 do
     for alg in scaffold
     do
-        python experiments.py --model=simple-cnn \
+        python experiments.py --model=resnet \
             --dataset=cifar10 \
             --alg=$alg \
             --lr=0.01 \
@@ -12,13 +12,13 @@ do
             --epochs=10 \
             --n_parties=10 \
             --rho=0.9 \
-            --comm_round=50 \
+            --comm_round=100 \
             --partition=$partition \
             --beta=0.5 \
             --device='cuda:0' \
             --datadir='./data/' \
-            --logdir="./logs/$alg/" \
-            --noise=0 \
+            --logdir="./logs/resnet/$alg/" \
+            --noise=0.1 \
             --sample=1.0 \
             --init_seed=0
     done
