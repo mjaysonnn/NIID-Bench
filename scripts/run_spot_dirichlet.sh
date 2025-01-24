2021a540d723607ea690ec0ec6e043d9c51c0e6e
mjs <mjs8612@gmail.com>
2025-01-24 02:38:29 +0000

    Update client configuration in run_spot_dirichlet.sh: increase regular clients to 20, adjust communication rounds to 100, and epochs to 25 for enhanced training setup

--- scripts/run_spot_dirichlet.sh
+++ scripts/run_spot_dirichlet.sh
@@ -7,7 +7,7 @@ DATASET="cifar10"
 MODEL="simple-cnn"
 
 N_PARTIES=100
-NUM_P=10 # Number of regular clients
+NUM_P=20 # Number of regular clients
 NUM_Q=5  # Number of partial update clients
 
 PARTITION="noniid-labeldir" # Change to "homo" if needed
@@ -17,8 +17,8 @@ BETA=0.5 # Dirichlet noise
 # PARTITION="homo" # Change to "homo" if needed
 # NOISE=0.1 # Homo noise -> use 0.0 if homo
 
-COMM_ROUNDS=500
-EPOCHS=10
+COMM_ROUNDS=100
+EPOCHS=25
 
 LR=0.01
 BATCH_SIZE=64
