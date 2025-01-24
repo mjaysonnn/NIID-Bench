51b37a81df2864d79585297b6aea98ff1e445d67
mjs <mjs8612@gmail.com>
2025-01-24 18:35:20 +0000

    Update run_spot_dirichlet.sh: adjust partial update clients to 5, streamline training parameters, and implement learning rate loop for enhanced experimentation

--- scripts/run_spot_dirichlet.sh
+++ scripts/run_spot_dirichlet.sh
@@ -8,19 +8,15 @@ MODEL="simple-cnn"
 
 N_PARTIES=100
 NUM_P=20 # Number of regular clients
-NUM_Q=15  # Number of partial update clients
+NUM_Q=5  # Number of partial update clients
 
 PARTITION="noniid-labeldir" # Change to "homo" if needed
 BETA=0.5 # Dirichlet noise
 
-# Comment if using noniid-labeldir
-# PARTITION="homo" # Change to "homo" if needed
-# NOISE=0.1 # Homo noise -> use 0.0 if homo
+COMM_ROUNDS=250
+LR_LIST=(0.001 0.1 1) # List of learning rates
+EPOCHS=10
 
-COMM_ROUNDS=500
-EPOCHS=5
-
-LR=0.01
 BATCH_SIZE=64
 RHO=0.9
 DEVICE="cuda:0"
@@ -45,23 +41,28 @@ echo "Algorithm: $ALG"
 # Define specific log directory for this algorithm
 LOGDIR="$LOGDIR_BASE/$MODEL/$ALG/$PARTITION/"
 
-# Run the experiment
-python experiments_spot.py \
-    --model=$MODEL \
-    --dataset=$DATASET \
-    --alg=$ALG \
-    --lr=$LR \
-    --batch-size=$BATCH_SIZE \
-    --epochs=$EPOCHS \
-    --n_parties=$N_PARTIES \
-    --rho=$RHO \
-    --comm_round=$COMM_ROUNDS \
-    --partition=$PARTITION \
-    --beta=$BETA \
-    --device=$DEVICE \
-    --datadir=$DATADIR \
-    --logdir=$LOGDIR \
-    --sample=$SAMPLE \
-    --init_seed=$INIT_SEED \
-    --p=$P \
-    --q=$Q
+# Loop through learning rates
+for LR in "${LR_LIST[@]}"; do
+    echo "Running experiment with learning rate: $LR"
+
+    # Run the experiment
+    python experiments_spot.py \
+        --model=$MODEL \
+        --dataset=$DATASET \
+        --alg=$ALG \
+        --lr=$LR \
+        --batch-size=$BATCH_SIZE \
+        --epochs=$EPOCHS \
+        --n_parties=$N_PARTIES \
+        --rho=$RHO \
+        --comm_round=$COMM_ROUNDS \
+        --partition=$PARTITION \
+        --beta=$BETA \
+        --device=$DEVICE \
+        --datadir=$DATADIR \
+        --logdir=$LOGDIR \
+        --sample=$SAMPLE \
+        --init_seed=$INIT_SEED \
+        --p=$P \
+        --q=$Q
+done
