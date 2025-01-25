0a6206ce790c5bebb8e8a590610a7fa001cd1554
mjs <mjs8612@gmail.com>
2025-01-25 21:10:37 +0000

    Update run_spot_dirichlet.sh: increase number of partial update clients from 10 to 15 for enhanced training capacity

--- scripts/run_spot_dirichlet.sh
+++ scripts/run_spot_dirichlet.sh
@@ -8,14 +8,15 @@ MODEL="simple-cnn"
 
 N_PARTIES=100
 NUM_P=20 # Number of regular clients
-NUM_Q=10  # Number of partial update clients
+NUM_Q=15  # Number of partial update clients
 
 PARTITION="noniid-labeldir" # Change to "homo" if needed
 BETA=0.5 # Dirichlet noise
 
 COMM_ROUNDS=100
-LR_LIST=(0.001 0.1 1) # List of learning rates
 EPOCHS=25
+
+LR_LIST=(0.001 0.1 1) # List of learning rates
 
 BATCH_SIZE=64
 RHO=0.9
