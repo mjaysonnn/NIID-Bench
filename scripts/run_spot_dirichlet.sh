bbababaea0aa9ee6014b977a99cdff88b344ed35
mjs <mjs8612@gmail.com>
2025-01-29 04:55:36 +0000

    Update run_spot_dirichlet scripts: change algorithm to scaffold, set partial update clients to 0, and adjust epochs and communication rounds

--- scripts/run_spot_dirichlet.sh
+++ scripts/run_spot_dirichlet.sh
@@ -8,13 +8,13 @@ MODEL="simple-cnn"
 
 N_PARTIES=100
 NUM_P=20 # Number of regular clients
-NUM_Q=15  # Number of partial update clients
+NUM_Q=0  # Number of partial update clients
 
 PARTITION="noniid-labeldir" # Change to "homo" if needed
 BETA=0.5 # Dirichlet noise
 
-EPOCHS=2
-COMM_ROUNDS=125
+EPOCHS=5
+COMM_ROUNDS=50
 
 LR_LIST=(0.01) # List of learning rates
 
