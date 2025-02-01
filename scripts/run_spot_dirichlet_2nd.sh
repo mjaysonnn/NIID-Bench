575852c1eb374467e65d7bf0d94134f09a47732c
mjs <mjs8612@gmail.com>
2025-02-01 04:24:40 +0000

    Update run_spot_dirichlet_2nd script: change partition to homo, set epochs to 1, and increase communication rounds to 500

--- scripts/run_spot_dirichlet_2nd.sh
+++ scripts/run_spot_dirichlet_2nd.sh
@@ -10,11 +10,13 @@ N_PARTIES=100
 NUM_P=20 # Number of regular clients
 NUM_Q=0  # Number of partial update clients
 
-PARTITION="noniid-labeldir" # Change to "homo" if needed
-BETA=0.5 # Dirichlet noise
+# PARTITION="noniid-labeldir" # Change to "homo" if needed
+BETA=0.5 # Dirichlet noise (but not used) 
 
-EPOCHS=5
-COMM_ROUNDS=50
+PARTITION="homo"
+
+EPOCHS=1
+COMM_ROUNDS=500
 
 LR_LIST=(0.01) # List of learning rates
 
