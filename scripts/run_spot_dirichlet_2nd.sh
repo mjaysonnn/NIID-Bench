8758970c2c553cb754b496878c2e8c425c6d8e6d
mjs <mjs8612@gmail.com>
2025-02-02 00:09:31 +0000

    Update run_spot_dirichlet scripts: change algorithm to fedavg, set partial update clients to 10 and 15, and adjust epochs and communication rounds to 1 and 500

--- scripts/run_spot_dirichlet_2nd.sh
+++ scripts/run_spot_dirichlet_2nd.sh
@@ -8,15 +8,15 @@ MODEL="simple-cnn"
 
 N_PARTIES=100
 NUM_P=20 # Number of regular clients
-NUM_Q=0  # Number of partial update clients
+NUM_Q=15  # Number of partial update clients
 
 # PARTITION="noniid-labeldir" # Change to "homo" if needed
-BETA=0.5 # Dirichlet noise
+BETA=0.5 # Dirichlet noise (but not used) 
 
 PARTITION="homo"
 
-EPOCHS=5
-COMM_ROUNDS=100
+EPOCHS=1
+COMM_ROUNDS=500
 
 LR_LIST=(0.01) # List of learning rates
 
