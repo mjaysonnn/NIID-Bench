efc296e20a2f1f4c0184a24ccc30aa10cda8f822
mjs <mjs8612@gmail.com>
2025-02-05 22:11:18 +0000

    Update run_spot_dirichlet script: set number of partial update clients to 0, increase epochs to 10, and reduce communication rounds to 250

--- scripts/run_spot_dirichlet.sh
+++ scripts/run_spot_dirichlet.sh
@@ -8,15 +8,15 @@ MODEL="simple-cnn"
 
 N_PARTIES=100
 NUM_P=20 # Number of regular clients
-NUM_Q=20  # Number of partial update clients
+NUM_Q=0  # Number of partial update clients
 
 # PARTITION="noniid-labeldir" # Change to "homo" if needed
 PARTITION="homo"
 
 BETA=0.5 # Dirichlet noise (but not used, if using homo)
 
-EPOCHS=5
-COMM_ROUNDS=500
+EPOCHS=10
+COMM_ROUNDS=250
 
 LR_LIST=(0.01) # List of learning rates
 
