0c86fc0f83690afa47577cf4e93db5453d97863b
mjs <mjs8612@gmail.com>
2025-02-07 04:38:57 +0000

    Update run_spot_dirichlet script: change model to resnet18, set number of partial update clients to 0, and adjust epochs to 1 and communication rounds to 2500

--- scripts/run_spot_dirichlet.sh
+++ scripts/run_spot_dirichlet.sh
@@ -4,19 +4,19 @@ ALG="fedavg" # Change to other algorithms if needed
 
 # Define variables for common parameters
 DATASET="cifar10"
-MODEL="simple-cnn"
+MODEL="resnet18"
 
 N_PARTIES=100
 NUM_P=20 # Number of regular clients
-NUM_Q=5  # Number of partial update clients
+NUM_Q=0  # Number of partial update clients
 
-# PARTITION="noniid-labeldir" # Change to "homo" if needed
-PARTITION="homo"
-
+PARTITION="noniid-labeldir" # Change to "homo" if needed
 BETA=0.5 # Dirichlet noise (but not used, if using homo)
 
-EPOCHS=10
-COMM_ROUNDS=250
+# PARTITION="homo"
+
+EPOCHS=1
+COMM_ROUNDS=2500
 
 LR_LIST=(0.01) # List of learning rates
 
